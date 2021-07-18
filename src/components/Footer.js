6d444ff65c621914da82cddde8a894f39385c55f
dmnv <daminov.arslan@gmail.com>
2021-07-19 02:22:44 +0600

    changed url

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer style={{ marginTop: '40px' }}>
-            <p>This page is made using React, and you can check it source code here: <a href="https://github.com/And1viper/simple-gachi-test">GitHub</a>. <br/>
+            <p>This page is made using React, and you can check it source code here: <a href="https://github.com/And1viper/simple-gachi-test/tree/master">GitHub</a>. <br/>
             Feel free to contribute! xDD</p>
         </footer>
     )
